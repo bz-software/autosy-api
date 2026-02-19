a085af26663f30a47dc5dd175744c51d77f76676
Vinicius <contato@vbony.xyz>
2026-02-19 19:20:37 -0300

    feat(CashTransaction) validating data

--- app/Http/Controllers/CashTransactionController.php
+++ app/Http/Controllers/CashTransactionController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\DTOs\CashTransactionDTO;
+use App\Http\Requests\CashTransaction\StoreCashTransactionRequest;
 use App\Http\Resources\CashTransaction\CashTransactionResource;
 use App\Services\CashTransactionService;
 use Illuminate\Http\Request;
+
+// use Illuminate\Http\Request;
 
 class CashTransactionController extends Controller
 {
     public function __construct(private CashTransactionService $service) {}
 
-    public function store(Request $request) {
+    public function store(StoreCashTransactionRequest $request) {
         return new CashTransactionResource(
             $this->service->store(
                 CashTransactionDTO::fromRequest($request),
--- app/Services/CashTransactionService.php
+++ app/Services/CashTransactionService.php
@@ -3,16 +3,25 @@
 namespace App\Services;
 
 use App\DTOs\CashTransactionDTO;
+use App\Exceptions\ServiceException;
 use App\Repositories\CashTransactionRepository;
+use Carbon\Carbon;
 
 class CashTransactionService
 {
     public function __construct(private CashTransactionRepository $repository){}
 
     public function store(CashTransactionDTO $dto, $idUser, $idWorkshop){
+        $transactionDate = Carbon::parse($dto->transaction_date);
+
+        $startOfCurrentMonth = Carbon::now()->startOfMonth();
+
+        if ($transactionDate->lt($startOfCurrentMonth)) {
+            throw new ServiceException([], 400, "Não é permitido lançar movimentações para meses anteriores.");
+        }
+
         $dto->created_by = $idUser;
         $dto->id_workshop = $idWorkshop;
-
         return $this->repository->create($dto->toArray());
     }
 }
