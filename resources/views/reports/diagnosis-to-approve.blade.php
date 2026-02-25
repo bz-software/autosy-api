a40539af18625ec53f90dc040a455af06bd60f73
Matheus Ferreira <matheus8142@hotmail.com>
2026-02-24 23:51:46 -0300

    Criando PDF de emissão de orçamento

--- resources/views/reports/diagnosis-to-approve.blade.php
+++ resources/views/reports/diagnosis-to-approve.blade.php
@@ -13,10 +13,47 @@
     </style>
 </head>
 <body>
-
-    <h1>Relatório para aprovação</h1>
-
-    <p><?= $id ?></p>
-    <p><?= $license_plate ?></p>
+    <div id="container">
+        <div id="container-cabeçalho">
+            <div id="cabeçalho">
+                <div>sua logo aqui</div>
+                <div id="container-dados">
+                    <div>Orçamento Autosy</div>
+                    <div>TELEFONE</div>
+                    <div>EMAIL</div>
+                    <div>WEBSITE</div>
+                </div>
+            </div>
+        </div>
+        <div id="container-2">
+            <div>Orçamento nº</div>
+            <div>emitido em</div>
+            <div>válido até</div>
+        </div>
+        <div id="container-3">
+            <div id="container-cliente">
+                <div>CLIENTE</div>
+            </div>
+            <div id="container-informacoes">
+                <div id="container-inf-1">
+                    <div>TELEFONE</div>
+                    <div>EMAIL</div>
+                </div>
+                <div id="container-inf-2">
+                    <div>CPF/CNPJ</div>
+                    <div>RG/IE</div>
+                </div>
+                <div id="container-inf-3">
+                    <div>ENDEREÇO</div>
+                </div>
+                <div id="container-inf-4">
+                    <div>BAIRRO</div>
+                    <div>CIDADE</div>
+                    <div>ESTADO</div>
+                    <div>CEP</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
