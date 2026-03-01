ec9ab7dcd3f265d6a46fcc9b086c5b602373bd93
Matheus Ferreira <matheus8142@hotmail.com>
2026-03-01 00:09:52 -0300

    adicionando tabelas

--- resources/views/reports/diagnosis-to-approve.blade.php
+++ resources/views/reports/diagnosis-to-approve.blade.php
@@ -25,24 +25,37 @@
                 </div>
             </div>
         </div>
-        <div id="container-2">
-            <div>Orçamento nº</div>
-            <div>emitido em</div>
-            <div>válido até</div>
-        </div>
-        <div id="container-3">
-            <div id="container-cliente">
-                <div>CLIENTE</div>
-            </div>
-            <div id="container-informacoes">
-                <div id="container-inf-1">
-                    <div>TELEFONE</div>
-                    <div>EMAIL</div>
-                </div>
-                <div id="container-inf-2">
-                    <div>CPF/CNPJ</div>
-                    <div>RG/IE</div>
-                </div>
+        <table border="1" id="container-2">
+            <tr>
+                <th> Orçamento nº</th>
+                <th colspan="1">999999</th>
+                <th>emitido em</th>
+                <th colspan="1">12/10/1998</th>
+                <th>válido até</th>
+                <th colspan="1">10/03/2026</th>
+            </tr>
+        </table>
+        <table border="1" id="container-3">
+            <tr id="container-cliente">
+                <th>CLIENTE</th>
+                <th colspan="1">MATHEUS DO SACRAMENTO</th>
+            </tr>
+            <table border="1" id="container-informacoes">
+                <tr id="container-inf-1">
+                    <th>TELEFONE</th>
+                    <th colspan="2">999-99999</th>
+                    <th>EMAIL</th>
+                    <th colspan="1">MATHEUS8142@HOTMAIL.COM</th>
+                </tr>
+            </table>
+            <table border="1" id="container-3">
+                <tr>
+                    <th>CPF/CNPJ</th>
+                     <th colspan="1">02448800189</th>
+                    <th>RG/IE</th>
+                     <th colspan="1">3434343434</th>
+                </tr>
+            </table>
                 <div id="container-inf-3">
                     <div>ENDEREÇO</div>
                 </div>
@@ -52,8 +65,7 @@
                     <div>ESTADO</div>
                     <div>CEP</div>
                 </div>
-            </div>
-        </div>
+        </table>
     </div>
 </body>
 </html>
