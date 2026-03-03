cb1a5b0164700548d8ece94b7bf340bb07b69960
Matheus Ferreira <matheus8142@hotmail.com>
2026-03-03 00:09:45 -0300

    (fix) Ajustando relatorio de aprovação

--- resources/views/reports/diagnosis-to-approve.blade.php
+++ resources/views/reports/diagnosis-to-approve.blade.php
@@ -40,32 +40,21 @@
                 <th>CLIENTE</th>
                 <th colspan="1">MATHEUS DO SACRAMENTO</th>
             </tr>
+        </table>
             <table border="1" id="container-informacoes">
                 <tr id="container-inf-1">
                     <th>TELEFONE</th>
                     <th colspan="2">999-99999</th>
-                    <th>EMAIL</th>
-                    <th colspan="1">MATHEUS8142@HOTMAIL.COM</th>
                 </tr>
             </table>
             <table border="1" id="container-3">
                 <tr>
-                    <th>CPF/CNPJ</th>
-                     <th colspan="1">02448800189</th>
-                    <th>RG/IE</th>
-                     <th colspan="1">3434343434</th>
+                    <th>PLACA</th>
+                    <th colspan="1">OVO5Q22</th>
+                    <th>CARRO</th>
+                    <th colspan="1">3434343434</th>
                 </tr>
             </table>
-                <div id="container-inf-3">
-                    <div>ENDEREÇO</div>
-                </div>
-                <div id="container-inf-4">
-                    <div>BAIRRO</div>
-                    <div>CIDADE</div>
-                    <div>ESTADO</div>
-                    <div>CEP</div>
-                </div>
-        </table>
     </div>
 </body>
 </html>
